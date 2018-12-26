f2e14d330b76509f2cbcc05c1bf4065e77daeccc
keshav22 <keshavagrawal21@gmail.com>
2018-12-26 18:12:40 +0530

    Add files via upload

---
+++ engine.py
@@ -0,0 +1,157 @@
+
+# coding: utf-8
+
+# In[15]:
+
+
+import librosa
+import os
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
+from keras.utils import to_categorical
+import numpy as np
+from sklearn.model_selection import train_test_split
+from tqdm import tqdm
+
+
+# In[6]:
+
+DATA_PATH = "C:/Users/kesha/Desktop/tts/WordRecognizer-master/data/"
+
+def get_labels(path=DATA_PATH):
+    labels = os.listdir(path)
+    label_indices = np.arange(0, len(labels))
+    return labels, label_indices, to_categorical(label_indices)
+
+def wav2mfcc(file_path, max_len=11):
+    wave, sr = librosa.load(file_path, mono=True, sr=None)
+    wave = wave[::3]
+    mfcc = librosa.feature.mfcc(wave, sr=16000)
+    if (max_len > mfcc.shape[1]):
+        pad_width = max_len - mfcc.shape[1]
+        mfcc = np.pad(mfcc, pad_width=((0, 0), (0, pad_width)), mode='constant')
+    else:
+        mfcc = mfcc[:, :max_len]
+    
+    return mfcc
+
+
+def save_data_to_array(path=DATA_PATH, max_len=11):
+    labels, _, _ = get_labels(path)
+
+    for label in labels:
+        mfcc_vectors = []
+        wavfiles = [path + label + '/' + wavfile for wavfile in os.listdir(path + '/' + label)]
+        for wavfile in tqdm(wavfiles, "Saving vectors of label - '{}'".format(label)):
+            mfcc = wav2mfcc(wavfile, max_len=max_len)
+            mfcc_vectors.append(mfcc)
+        np.save(label + '.npy', mfcc_vectors)
+
+
+def get_train_test(split_ratio=0.6, random_state=42):
+    labels, indices, _ = get_labels(DATA_PATH)
+    X = np.load(labels[0] + '.npy')
+    y = np.zeros(X.shape[0])
+    for i, label in enumerate(labels[1:]):
+        x = np.load(label + '.npy')
+        X = np.vstack((X, x))
+        y = np.append(y, np.full(x.shape[0], fill_value= (i + 1)))
+    assert X.shape[0] == len(y)
+    return train_test_split(X, y, test_size= (1 - split_ratio), random_state=random_state, shuffle=True)
+
+
+
+def prepare_dataset(path=DATA_PATH):
+    labels, _, _ = get_labels(path)
+    data = {}
+    for label in labels:
+        data[label] = {}
+        data[label]['path'] = [path  + label + '/' + wavfile for wavfile in os.listdir(path + '/' + label)]
+        vectors = []
+        for wavfile in data[label]['path']:
+            wave, sr = librosa.load(wavfile, mono=True, sr=None)
+            wave = wave[::3]
+            mfcc = librosa.feature.mfcc(wave, sr=16000)
+            vectors.append(mfcc)
+        data[label]['mfcc'] = vectors
+    return data
+
+
+def load_dataset(path=DATA_PATH):
+    data = prepare_dataset(path)
+    dataset = []
+    for key in data:
+        for mfcc in data[key]['mfcc']:
+            dataset.append((key, mfcc))
+    return dataset[:100]
+
+
+# In[7]:
+
+
+feature_dim_2 = 11
+save_data_to_array(max_len=feature_dim_2)
+X_train, X_test, y_train, y_test = get_train_test()
+
+feature_dim_1 = 20
+channel = 1
+epochs = 50
+batch_size = 100
+verbose = 1
+num_classes = 3  # Change number of classes
+
+X_train = X_train.reshape(X_train.shape[0], feature_dim_1, feature_dim_2, channel)
+X_test = X_test.reshape(X_test.shape[0], feature_dim_1, feature_dim_2, channel)
+
+y_train_hot = to_categorical(y_train)
+y_test_hot = to_categorical(y_test)
+
+
+# In[8]:
+
+
+def get_model():
+    model = Sequential()
+    model.add(Conv2D(32, kernel_size=(2, 2), activation='relu', input_shape=(feature_dim_1, feature_dim_2, channel)))
+    model.add(Conv2D(48, kernel_size=(2, 2), activation='relu'))
+    model.add(Conv2D(120, kernel_size=(2, 2), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu'))
+    model.add(Dropout(0.25))
+    model.add(Dense(64, activation='relu'))
+    model.add(Dropout(0.4))
+    model.add(Dense(num_classes, activation='softmax'))
+    model.compile(loss=keras.losses.categorical_crossentropy,
+                  optimizer=keras.optimizers.Adadelta(),
+                  metrics=['accuracy'])
+    return model
+
+def predict(filepath, model):
+    sample = wav2mfcc(filepath)
+    sample_reshaped = sample.reshape(1, feature_dim_1, feature_dim_2, channel)
+    return get_labels()[0][
+            np.argmax(model.predict(sample_reshaped))
+    ]
+
+
+# In[9]:
+
+
+model = get_model()
+model.fit(X_train, y_train_hot, batch_size=batch_size, epochs=epochs, verbose=verbose, validation_data=(X_test, y_test_hot))
+
+
+# In[4]:
+
+
+import os
+list = os.listdir("./Words/") # dir is your directory path
+
+
+for i in list :
+    print(predict('./Words/'+i, model=model))
+
+
---
+++ first.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 22 02:20:27 2018
+
+@author: keshav
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 21 23:11:34 2018
+
+@author: keshav
+"""
+
+import pyaudio
+import wave
+from array import array
+#from matplotlib import pyplot as plt
+FORMAT=pyaudio.paInt16
+CHANNELS=2
+RATE=16000
+CHUNK=1024
+RECORD_SECONDS=6
+FILE_NAME="sentence"
+
+audio=pyaudio.PyAudio() 
+
+stream=audio.open(format=FORMAT,channels=CHANNELS, 
+                  rate=RATE,
+                  input=True,
+                  frames_per_buffer=CHUNK)
+
+frames=[]
+i1=0
+way=0
+for i in range(0,int(RATE/CHUNK*RECORD_SECONDS)):
+    data=stream.read(CHUNK)
+    data_chunk=array('h',data)
+    frames.append(data)             
+    
+wavfile=wave.open(FILE_NAME+".wav",'wb')
+wavfile.setnchannels(CHANNELS)
+wavfile.setsampwidth(audio.get_sample_size(FORMAT))
+wavfile.setframerate(RATE)
+wavfile.writeframes(b''.join(frames))
+wavfile.close()
+            
+
+stream.stop_stream()
+stream.close()
+audio.terminate()
+#Importing pyplot
+
+
+#Plotting to our canvas
+#plt.plot(data_chunk)ó
+
+#Showing what we plotted
+#plt.show()
